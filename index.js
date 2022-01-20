d0c235083a246ce5086c7ddc2d5cac3918678a6f
shamimReza <shamimreza23393@gmail.com>
2022-01-20 11:02:02 +0600

    Genius commit

---
+++ index.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const { MongoClient } = require("mongodb");
+const ObjectId = require("mongodb").ObjectId;
+
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT ||5000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.z8q3a.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("carMechanic");
+    const servicesCollection = database.collection("services");
+    // GET API
+    app.get("/services", async (req, res) => {
+      const cursor = servicesCollection.find({});
+      const service = await cursor.toArray();
+      res.send(service);
+    });
+
+    // GET SINGLE Service
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await servicesCollection.findOne(query);
+      res.json(service);
+    });
+    //  POST API
+    app.post("/services", async (req, res) => {
+      const service = req.body;
+
+      console.log(service);
+      const result = await servicesCollection.insertOne(service);
+      res.json(result);
+    });
+    //DELETE API
+    app.delete('/services/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: ObjectId(id) };
+        const result = await servicesCollection.deleteOne(query);
+        res.json(result);
+    })
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir),
+  app.get("/", (req, res) => {
+    res.send("Running Genius Server");
+  });
+
+app.listen(port, () => {
+  console.log("Running Genius Server port ON : ", port);
+});
